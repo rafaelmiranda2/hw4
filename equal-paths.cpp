4f62658e7bb8289f8bad36d54390fe55608da97c
Rafael Miranda <rafaelmi@usc.edu>
2025-04-04 19:03:44 -0700

    Committed equalpaths

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,11 +8,45 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+int getHeight(Node* currNode) {
+    if (currNode == nullptr) {
+        return 0;
+    }
+    int leftHeight = getHeight(currNode->left);
+    int rightHeight = getHeight(currNode->right);
+
+    if (leftHeight >= rightHeight) {
+        return leftHeight + 1;
+    }
+    if (rightHeight >= leftHeight) {
+        return rightHeight + 1;
+    }
+    return 0;
+}
 
 
-bool equalPaths(Node * root)
+bool equalPaths(Node* root)
 {
     // Add your code below
+    if (root == nullptr) { //Base case
+        return true;
+    }
+    if (root->right == nullptr) {
+        if (root->left != nullptr && getHeight(root->left) == 1) {
+            return true;
+        }
+    }
+    if (root->left == nullptr) {
+        if (root->right != nullptr && getHeight(root->right) == 1) {
+            return true;
+        }
+    }
 
+    if (getHeight(root->left) == getHeight(root->right)) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
